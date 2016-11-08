adaddd91c550536448f49268c6c84cefc09cc98b
Ian rizki <ian.rizki2@gmail.com>
2016-11-08 11:51:27 +0700

    a

---
+++ artikel2/about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<link rel="stylesheet" type="text/css" href="../style/style.css">
+	<!-- Icon here -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+	<!-- font here -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+	<!-- inline style -->
+	<link rel="stylesheet" type="text/css" href="about.css">
+
+</head>
+<body>
+<?php include("navbar.html") ?>
+
+<div class="about">
+	<h2>Apa Yang kami kerjakan?</h2>
+	<p>
+		Kami membuat sebuah website untuk tempat saling berbagi resep makanan.
+	</p>
+	<br class="clear">
+	<h2>Mengapa kami melakukan ini?</h2>
+	<p>
+		Karena kami merasakan masih minimnya website khusus untuk tempat bagi para pencipta 
+		maupun pencinta makanan untuk membagikan resepnya.
+	</p>
+	<h2>Team kami</h2>
+	<div class="team">
+			<div class="people">
+				<img src="../images/user/proto.jpg">
+				<p class="nama">Philip lambok</p>
+				<p class="profesi">Programmer</p>
+			</div>
+			<div class="people-tengah1">
+				<img src="../images/user/ian.jpg">
+				<p class="nama">Ian rizki</p>
+				<p class="profesi">Programmer</p>
+			</div>
+			<div class="people-tengah2">
+				<img src="../images/user/yos.jpg">
+				<p class="nama">Yosua kelvin</p>
+				<p class="profesi">Administrator</p>
+			</div>
+			<div class="people">
+				<img src="../images/user/git.png">
+				<p class="nama">Maharani</p>
+				<p class="profesi">Designer</p>
+			</div>
+	</div>
+	<br class="clear">
+	<br class="clear">
+</div>
+<?php include("footer.html") ?>
+</body>
+</html>
