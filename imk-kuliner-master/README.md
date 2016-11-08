21f2e9fbae25ed057846555ae68cfc1048756199
Ian rizki <ian.rizki2@gmail.com>
2016-11-08 11:25:36 +0700

    Instalasi
    clone atau download repository.
    Pastikan sudah mempunyai xampp(server PHP) atau sejenisnya.
    Simpan folder repo pada htdocs
    Jalankan dengan url. contoh : localhost/imk-kuliner

---
+++ imk-kuliner-master/README.md
@@ -0,0 +1,7 @@
+# imk-kuliner
+
+## Instalasi
+1. clone atau download repository.
+2. Pastikan sudah mempunyai xampp(server PHP) atau sejenisnya.
+3. Simpan folder repo pada htdocs
+4. Jalankan dengan url. contoh : localhost/imk-kuliner 
---
+++ imk-kuliner-master/artikel/burger.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Burger</title>
+
+	<!-- Video.js here -->
+	<link href="http://vjs.zencdn.net/5.11.8/video-js.css" rel="stylesheet">
+
+	<!-- Main style -->
+	<link rel="stylesheet" type="text/css" href="../style/style.css">
+
+	<!-- Artikel style -->
+	<link rel="stylesheet" type="text/css" href="artikel.css">
+
+	<!-- love imoutos -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+	<!-- font here -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+
+	
+</head>
+<body>
+<?php include "../navbar.html" ?>
+<div class="container">
+
+	<div class="produk">
+		<div class="keterangan">
+			<h2>Burger Oishi Desuu!!</h2>
+			<hr>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</div>
+	<img src="../images/food/2.jpg">
+	</div>
+
+	<br class="clear">
+	<br class="clear">
+	
+	<div class="resep">
+	<div class="bahan">
+		<h3>Bahan - Bahan</h3>
+		<p>
+		<ul>
+			<li>Daging</li>
+			<li>Roti</li>
+			<li>Sambal</li>
+			<li>Tepung</li>
+			<li>Mentega</li>
+			<li>Toge</li>
+			<li>Sawi</li>
+			<li>Timun</li>
+		</ul>
+		</p>
+	</div>
+	<div class="video">
+		<video width="100%" controls preload="auto">
+			<source src="../videos/test.mp4" type="video/mp4">
+		</video>
+	</div>
+	<br class="clear">
+		<h3>Cara Memasak</h3>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
+</div>
+
+<br class="clear">
+
+<?php include "../footer.html" ?>
+
+<script src="http://vjs.zencdn.net/5.11.8/video.js"></script>
+</body>
+</html>
